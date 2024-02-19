71c0f5aa9f220c850400c2d0b2154f347c041921
samer025 <samer.amor@esprit.tn>
2024-02-20 00:01:12 +0100

    'entities'

---
+++ Backendspr/src/main/java/com/example/backendspr/BackendsprApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendspr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendsprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendsprApplication.class, args);
+	}
+
+}
---
+++ Backendspr/src/test/java/com/example/backendspr/BackendsprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendspr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendsprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
